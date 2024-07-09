76720ce8bc6155f0c04b3275d9c412c244866307
MISAEL LEZMA MESINO <misael.lezma@coppel.com>
2024-07-09 13:24:05 -0600

    Se agrega pantalla para la generacion de hash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [folio, setFolio] = useState('');
+    const [hash, setHash] = useState('');
+    const [error, setError] = useState('');
+
+    const handleClick = async () => {
+        try {
+            const response = await axios.post('http://localhost:3033/gethash', { folio });
+            setHash(response.data.hash);
+            setError('');
+        } catch (error) {
+            console.error('Error al consumir el servicio de folios', error);
+            setError('Error al consumir el servicio de folios');
+            setHash('');
+        }
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Folios Apps</h1>
+                <div>
+                    <label htmlFor="folioInput">Folio:</label>
+                    <input
+                        id="folioInput"
+                        type="text"
+                        value={folio}
+                        onChange={(e) => setFolio(e.target.value)}
+                        placeholder="Ingresar folio"
+                    />
+                    <button onClick={handleClick}>Generar Hash</button>
+                </div>
+                {error && <p>{error}</p>}
+                {hash && <p>Hash: {hash}</p>}
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
